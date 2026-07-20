57a1dadfb153a6f6ff22a90e7e4c8b92b7f677df
Sahil Mujawar <sahil@nadsoftdesign.com>
2026-07-20 22:42:44 +0530

    Add middleware interface and authentication middleware

---
+++ app/Middleware/AuthMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Middleware;
+
+use App\Core\Response;
+use App\Core\Session;
+
+class AuthMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private Session $session,
+        private Response $response
+    ) {
+    }
+
+    public function handle(): void
+    {
+        if (!$this->session->has('user_id')) {
+            $this->response->redirect('/login');
+        }
+    }
+}
