dc93aadced8ec3f38a6e3644531dac9dd8ddc5ed
Sahil Mujawar <sahil@nadsoftdesign.com>
2026-07-21 17:27:30 +0530

    Add support for dynamic route parameters

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -49,14 +49,19 @@ class Router
     {
         $method = $_SERVER['REQUEST_METHOD'];
 
-        if (!isset($this->routes[$method][$uri])) {
-            http_response_code(404);
-            require APP_PATH . '/Views/errors/404.php';
-            exit;
-        }
+        // if (!isset($this->routes[$method][$uri])) {
+        //     http_response_code(404);
+        //     require APP_PATH . '/Views/errors/404.php';
+        //     exit;
+        // }
 
-        /** @var Route $route */
-        $route = $this->routes[$method][$uri];
+        // /** @var Route $route */
+        // $route = $this->routes[$method][$uri];
+        $route = $this->findRoute($method, $uri);
+
+        if (!$route) {
+            throw new \Exception('Route not found');
+        }
 
         [$controller, $action] = $route->action;
 
@@ -81,12 +86,79 @@ class Router
 
             $type = $parameter->getType();
 
-            if ($type instanceof ReflectionNamedType && !$type->isBuiltin()) {
+            // Class dependency
+            if ($type && !$type->isBuiltin()) {
+
                 $dependencies[] = $container->make($type->getName());
+                continue;
             }
 
+            // Route parameter
+            $name = $parameter->getName();
+
+            if (isset($route->parameters[$name])) {
+                $dependencies[] = $route->parameters[$name];
+                continue;
+            }
+
+            // Default value
+            if ($parameter->isDefaultValueAvailable()) {
+                $dependencies[] = $parameter->getDefaultValue();
+                continue;
+            }
+
+            throw new \Exception("Unable to resolve parameter '{$name}'");
         }
 
         $reflectionMethod->invokeArgs($instance, $dependencies);
+    }
+
+    private function findRoute(string $method, string $uri): ?Route
+    {
+        if (!isset($this->routes[$method])) {
+            return null;
+        }
+
+        foreach ($this->routes[$method] as $route) {
+
+            // Exact match
+            if ($route->uri === $uri) {
+                return $route;
+            }
+            $routeParts = explode('/', trim($route->uri, '/'));
+            $uriParts = explode('/', trim($uri, '/'));
+
+            // Number of segments must match
+            if (count($routeParts) !== count($uriParts)) {
+                continue;
+            }
+            $parameters = [];
+            $matched = true;
+            
+            // Segment matching loop
+            foreach ($routeParts as $index => $part) {
+                // Dynamic parameter
+                if (preg_match('/^\{(.+)\}$/', $part, $matches)) {
+                    $parameters[$matches[1]] = $uriParts[$index];
+                    continue;
+                }
+
+                // Static segment must match
+                if ($part !== $uriParts[$index]) {
+                    $matched = false;
+                    break;
+                }
+
+            }
+
+            // Return immediately if this route matched (FIXED LOCATION)
+            if ($matched) {
+                $route->parameters = $parameters;
+
+                return $route;
+            }
+        }
+
+        return null;
     }
 }
